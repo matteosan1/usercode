e75a693b098280a54ed66f761e5d0fecf03d6a91
sani <>
2007-11-01 18:39:11 +0000

    Added conversion

--- NtupleMakers/ElectronAnalyzer/src/SealModule.cc
+++ NtupleMakers/ElectronAnalyzer/src/SealModule.cc
@@ -2,9 +2,13 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "NtupleMakers/ElectronAnalyzer/interface/NewElectrons.h"
 #include "NtupleMakers/ElectronAnalyzer/interface/NewElectronsB.h"
+#include "NtupleMakers/ElectronAnalyzer/interface/NewElectronsC.h"
+#include "NtupleMakers/ElectronAnalyzer/interface/Conversion.h"
 #include "NtupleMakers/ElectronAnalyzer/interface/SeedEfficiency.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(NewElectrons);
-DEFINE_ANOTHER_FWK_MODULE(NewElectronsB);
+DEFINE_ANOTHER_FWK_MODULE(NewElectrons);  // for efficiency
+DEFINE_ANOTHER_FWK_MODULE(NewElectronsB); // for fake rate
+DEFINE_ANOTHER_FWK_MODULE(NewElectronsC); // for Claude studies
+DEFINE_ANOTHER_FWK_MODULE(Conversion);    // to study conversion
 DEFINE_ANOTHER_FWK_MODULE(SeedEfficiency);
