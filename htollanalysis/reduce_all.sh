42d62f82a01a4c01fd739f1ef5fb1d8835e05ca3
capalmer <>
2013-01-08 16:31:16 +0000

    all samples to be reduced for mumu

---
+++ htollanalysis/reduce_all.sh
@@ -0,0 +1,50 @@
+
+./Matteo/reduce_batch Hmm                       HtollAnalysis -1  Hmm                                                                                                 /hadoop/cms/store/user/matteo/Summer12/Htoll/Hmm       htollanalysis/htollanalysis.dat htollanalysis/reduction_input.dat htollanalysis/reduction_output.dat
+./Matteo/reduce_batch RSmm                      HtollAnalysis -1  RSmm                                                                                                /hadoop/cms/store/user/matteo/Summer12/Htoll/RSmm      htollanalysis/htollanalysis.dat htollanalysis/reduction_input.dat htollanalysis/reduction_output.dat
+./Matteo/reduce_batch VBFmm                     HtollAnalysis -1  VBFmm                                                                                               /hadoop/cms/store/user/matteo/Summer12/Htoll/VBFmm     htollanalysis/htollanalysis.dat htollanalysis/reduction_input.dat htollanalysis/reduction_output.dat
+
+./Matteo/reduce_batch DYJetsToLL_M-50_2MuonSkim HtollAnalysis 1   DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1_2ElectronSkim /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1_2ElectronSkim        htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+
+
+
+
+./Matteo/reduce_batch     QCD_EN_MU_120170 HtollAnalysis 1   QCD_EN_MU_120170    /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_120170    htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_1520   HtollAnalysis 1   QCD_EN_MU_1520      /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_1520      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_170300 HtollAnalysis 1   QCD_EN_MU_170300    /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_170300    htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_2030   HtollAnalysis 1   QCD_EN_MU_2030      /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_2030      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_300470 HtollAnalysis 1   QCD_EN_MU_300470    /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_300470    htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_3050   HtollAnalysis 1   QCD_EN_MU_3050      /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_3050      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_5080   HtollAnalysis 1   QCD_EN_MU_5080      /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_5080      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     QCD_EN_MU_80120  HtollAnalysis 1   QCD_EN_MU_80120     /hadoop/cms/store/user/matteo/Summer12/QCD_EN_MU_80120     htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WJetsMM          HtollAnalysis 1   WJetsMM             /hadoop/cms/store/user/matteo/Summer12/WJetsMM             htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+
+
+
+./Matteo/reduce_batch        run2012A_DM                  HtollAnalysis 0      run2012A_DM                     /hadoop/cms/store/user/matteo/data/2012_2/run2012A_DM                      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch        run2012C_DM                  HtollAnalysis 0      run2012C_DM                     /hadoop/cms/store/user/matteo/data/2012_2/run2012C_DM                      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch        run2012A_recovery_DM         HtollAnalysis 0      run2012A_recovery_DM            /hadoop/cms/store/user/matteo/data/2012_2/run2012A_recovery_DM             htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch        run2012C_recovery_DM         HtollAnalysis 0      run2012C_recovery_DM            /hadoop/cms/store/user/matteo/data/2012_2/run2012C_recovery_DM             htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch        run2012B_DM                  HtollAnalysis 0      run2012B_DM                     /hadoop/cms/store/user/matteo/data/2012_2/run2012B_DM                      htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+
+./Matteo/reduce_batch     TTJets_FullLeptMGDecays_2MuonSkim           HtollAnalysis 1      TTJets_FullLeptMGDecays    /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/TTJets_FullLeptMGDecays_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v2_2MuonSkim                       htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WWJetsTo2L2Nu_2MuonSkim                     HtollAnalysis 1      WWJetsTo2L2Nu              /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WWJetsTo2L2Nu_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim               htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WWTo2L2Nu_2MuonSkim                         HtollAnalysis 1      WWTo2L2Nu                  /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WWTo2L2Nu_TuneZ2star_8TeV_pythia6_tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                    htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WZJetsTo2L2Q_2MuonSkim                      HtollAnalysis 1      WZJetsTo2L2Q               /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WZJetsTo2L2Q_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WZJetsTo2Q2Nu_2MuonSkim                     HtollAnalysis 1      WZJetsTo2Q2Nu              /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WZJetsTo2Q2Nu_TuneZ2star_8TeV-madgraph-tauloa_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim               htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WZJetsTo3LNu_2MuonSkim                      HtollAnalysis 1      WZJetsTo3LNu               /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WZJetsTo3LNu_TuneZ2_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                    htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     WZTo3LNu_2MuonSkim                          HtollAnalysis 1      WZTo3LNu                   /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/WZTo3LNu_TuneZ2star_8TeV_pythia6_tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                     htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     ZZJetsTo2L2Q_2MuonSkim                      HtollAnalysis 1      ZZJetsTo2L2Q               /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/ZZJetsTo2L2Q_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     ZZJetsTo4L_2MuonSkim                        HtollAnalysis 1      ZZJetsTo4L                 /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                  htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+./Matteo/reduce_batch     ZZTo4L_2MuonSkim                            HtollAnalysis 1      ZZTo4L                     /hadoop/cms/store/user/capalmer/V14_00_05/mc/Summer12_8TeV_S10/ZZTo4L_TuneZ2star_8TeV_pythia6_tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_2MuonSkim                       htollanalysis/htollanalysis.dat       htollanalysis/reduction_input.dat       htollanalysis/reduction_output.dat
+
+
+
+
+
+
+
+
+
+
+
+
